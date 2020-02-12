a55381f84b3170f4065cffa37366f67d77b7b2f6
Jarosław Steckiewicz <steckiewicz.jaroslaw@gmail.com>
2020-02-12 16:29:49 +0100

    Handle errors

--- server.js
+++ server.js
@@ -14,16 +14,20 @@ const start = function () {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.post('/subscribe', async function (req, res) {
-    const email = req.body.email;
-    await database('emails').insert({ email });
-    res.send("Your subscription successfully added!");
+    try {
+      const email = req.body.email;
+      await database('emails').insert({ email });
+      return res.send("Your subscription successfully added!");
+    } catch (e) {
+      res.send(e)
+    }
   })
 
   app.get('/email', async function (req, res) {
     const databaseItems = await database('emails').select();
     res.send(databaseItems)
   })
-  
+
   const port = 19511;
   app.listen(port);
   console.log('Application listen on port', port)
